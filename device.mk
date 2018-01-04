a0a429979afc68d91548f2b766f76c9da8802719
dianlujitao <dianlujitao@lineageos.org>
2018-01-04 09:48:59 +0000

    vayu: Build dummy android.hidl.[base,manager]@1.0
    [daniml3]: convert to blueprint
    
    Change-Id: I81a081130869c4d92f30b2674e75bb6b096a7a2c

--- device.mk
+++ device.mk
@@ -13,3 +13,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # File systems table
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0
