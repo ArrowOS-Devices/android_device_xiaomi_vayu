953a09eb9a409eb1242c8f9cca4972a023f8fc97
Rashed Abdel-Tawab <rashed@linux.com>
2018-05-09 13:16:27 -0700

    vayu: Build vendor version of libstdc++
    Change-Id: Ie43c0a6b594ecfcff393efb8a3937f04af0ee0a4

--- device.mk
+++ device.mk
@@ -109,6 +109,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+PRODUCT_PACKAGES += \
+   libstdc++.vendor
+
 # Config Store
 PRODUCT_PACKAGES += \
     disable_configstore
