35cd133d113ee3ecb3e8a99cb96dd693f8742176
Bowgo Tsai <bowgotsai@google.com>
2019-04-08 18:28:48 +0800

    vayu: Adding GSI keys
    Change-Id: I112ece91566e6cea066cd637bfed5cae6761ae66

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Shipping level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
