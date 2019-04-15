6def0d078c392a624e69ca90447c0dd199c8c4aa
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    vayu: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating
    of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Shipping level
 PRODUCT_SHIPPING_API_LEVEL := 30
 
