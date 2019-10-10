a62bf8b3df469dcfda4b2c7fa61c454cff8433eb
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 10:21:46 -0700

    vayu: Use component-override to configure sunfish Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default
    on sunfish devices.
    
    Remove usages of profile_supported_* from overlays as they no
    longer configure the enable state of Bluetooth componenets.
    
    Bug: 135048762
    Test:
          adb shell pm query-services \
          com.android.bluetooth/.avrcp.AvrcpTargetService
          adb shell pm query-services \
          com.android.bluetooth/.avrcpcontroller.AvrcpControllerService
          adb shell pm query-services \
          com.android.bluetooth/.ba.BATService
          adb shell pm query-services \
          com.android.bluetooth/.hid.HidDeviceService
          adb shell pm query-services \
          com.android.bluetooth/.hid.HidHostService
          adb shell pm query-services \
          com.android.bluetooth/.sap.SapService
    
    Change-Id: If41df9fdefbcd952f8387451dc137565343e3d75

--- device.mk
+++ device.mk
@@ -93,6 +93,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
