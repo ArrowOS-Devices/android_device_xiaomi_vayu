e55ce39477efaebc7d9788deace52a8eb5c85a1a
Peter Cai <peter@typeblog.net>
2020-02-27 21:13:21 +0800

    vayu: Panic to recovery not bootloader
    Change-Id: Iaf737890e3c860df504629d35a4a68c494f39910

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
