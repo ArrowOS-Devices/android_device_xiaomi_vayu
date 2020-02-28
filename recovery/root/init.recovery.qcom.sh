8dee840fb90fca47b1c758bf8c526bd5aa5ea7e8
Peter Cai <peter@typeblog.net>
2020-02-28 16:40:22 +0100

    vayu: Add Recovery support
    Change-Id: I5126d0d327a4008615088d0f1da36e364a6db3ab

---
+++ recovery/root/init.recovery.qcom.sh
@@ -0,0 +1,9 @@
+#!/system/bin/sh
+# Wait for some time to avoid us being overridden
+sleep 5
+# Make symlinks from /dev/block/mapper to /dev/block/bootdevice/by-name
+# For compatibility with mods like Magisk and OpenGapps
+# (vendor and system should be more than enough)
+ln -sf /dev/block/mapper/system /dev/block/bootdevice/by-name/system
+ln -sf /dev/block/mapper/vendor /dev/block/bootdevice/by-name/vendor
+
