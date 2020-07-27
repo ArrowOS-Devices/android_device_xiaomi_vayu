6bb0e35684d7586f4ae35d698ecd8a922a8bcec5
Justin Yun <justinyun@google.com>
2020-07-27 17:20:08 +0900

    vayu: Enforce product interfaces
    Add 'PRODUCT_PRODUCT_VNDK_VERSION := current' and
    'PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true' to enforce
    product interfaces
    
    Change-Id: I25436ecc8b4f2e90ed54b46fd976c66d897348db

--- device.mk
+++ device.mk
@@ -4,6 +4,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Enforce native interfaces of product partition as VNDK
+PRODUCT_PRODUCT_VNDK_VERSION := current
+
+# Enforce java interfaces of product partition
+PRODUCT_ENFORCE_PRODUCT_PARTITION_INTERFACE := true
+
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
