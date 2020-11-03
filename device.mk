a616d14a95748eebb9c143db2b58b9e012a0e0e3
Arvind Kumar <karvin@codeaurora.org>
2020-11-03 10:10:23 +0530

    vayu: Add vndservicemanager explicitly
    This is now added to product_packages based on shipping_api which is
    not yet supported on few targets. Hence adding it explicitly
    
    Change-Id: I4bc6d43627e92e1d7a7d3fe82ec6e58fc5fdaca7

--- device.mk
+++ device.mk
@@ -352,6 +352,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)//etc/excluded-input-devices.xml
 
+# Vndservicemanager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
