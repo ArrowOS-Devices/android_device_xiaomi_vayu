67aa4991c2008491e5b37852927e45944bfdd15b
TH779 <tongsj2333@gmail.com>
2021-01-01 18:58:34 +0800

    vayu: Import audio policy configuration from LA.UM.9.12.r1-09500-SMxx50.0
     * The audio policy configuration of the stock ROM
       will cause the wired headset to be unusable.
     * Also compile: audio.a2dp.default, libaacwrapper.
    
    Signed-off-by: TH779 <tongsj2333@gmail.com>
    Change-Id: I674c4088e87ea7a6ca22affcfc25772d205a64e6

--- device.mk
+++ device.mk
@@ -32,6 +32,15 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    libaacwrapper
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_policy_configuration.xml
+
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
