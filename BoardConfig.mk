742fbe0e3efaccc1f32e2084444b865b9b65776e
Arne Coucheron <arco68@gmail.com>
2021-01-12 05:45:19 +0100

    vayu: Silence clang warnings during kernel compile
    clang-11: warning: argument unused during compilation:
    '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -65,6 +65,7 @@ ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_CONFIG := vayu_user_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
+  TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 endif
 
 # Partitions
