888872cf2efc6b0beb5100a22b7eb9481a067179
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:02:44 +0100

    vayu: Build libhwbinder
    * Some libs uses it
    
    Change-Id: Ic40f730a4d498725459785b8c33ef7f5e8d0b13b

--- device.mk
+++ device.mk
@@ -206,7 +206,8 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhwbinder.vendor
 
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
