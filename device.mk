545ea3047476e32c61e4d59cdb2ad676cf1c945a
Bruno Martins <bgcngm@gmail.com>
2021-02-16 13:44:18 +0000

    vayu: Migrate to Xiaomi power AIDL HAL
    Change-Id: I42170d51a517170b58d532addd9c38496e43457c

--- device.mk
+++ device.mk
@@ -353,9 +353,12 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
+    android.hardware.power-service.xiaomi-libperfmgr \
     vendor.qti.hardware.perf@2.2.vendor
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Public libraries
 PRODUCT_COPY_FILES += \
