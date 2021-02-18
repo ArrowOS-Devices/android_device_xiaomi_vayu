618fe341b820042f3fba39e3f34e3a748fbc80a7
Chenyang Zhong <zhongcy95@gmail.com>
2021-02-18 07:49:01 -0500

    vayu: Set required props for userspace LMKD
    Android11 introduces an improved userspace lmkd by using "levels
    and thrashing to prevent memory starvation and performance
    degradation" [1]. Let's try this new mechanism by dropping
    simple_lmk in kernel, bringing back MEMCG/PSI, and setting up
    props. According to Google, default values for lmkd properties
    with the new strategy are good enough and property overrides are
    unnecessary [2].
    
    Test: lmkd_unit_test
    
    lmkd_unit_test results:
    
    [==========] Running 1 test from 1 test suite.
    [----------] Global test environment set-up.
    [----------] 1 test from lmkd
    [ RUN      ] lmkd.check_for_oom
    
    [  INFO ] system/memory/lmkd/tests/lmkd_test.cpp:271::
    Child [pid=21071] is running at oomadj=1000
    Child [pid=21071] allocated 6552MB before being killed
    Child [pid=21172] is running at oomadj=900
    Child [pid=21172] allocated 6696MB before being killed
    Child [pid=21235] is running at oomadj=800
    Child [pid=21235] allocated 6761MB before being killed
    Child [pid=21275] is running at oomadj=700
    Child [pid=21275] allocated 6105MB before being killed
    Child [pid=21401] is running at oomadj=600
    Child [pid=21401] allocated 6823MB before being killed
    Child [pid=21561] is running at oomadj=500
    Child [pid=21561] allocated 6839MB before being killed
    Child [pid=21585] is running at oomadj=400
    Child [pid=21585] allocated 6784MB before being killed
    Child [pid=21755] is running at oomadj=300
    Child [pid=21755] allocated 6768MB before being killed
    Child [pid=22040] is running at oomadj=200
    Child [pid=22040] allocated 6799MB before being killed
    Child [pid=22275] is running at oomadj=100
    Child [pid=22275] allocated 6985MB before being killed
    Child [pid=22403] is running at oomadj=0
    Child [pid=22403] allocated 7160MB before being killed
    
    [  INFO ] system/memory/lmkd/tests/lmkd_test.cpp:343:: ====Logged kills====
    ...
    
    [       OK ] lmkd.check_for_oom (234286 ms)
    [----------] 1 test from lmkd (234286 ms total)
    
    [----------] Global test environment tear-down
    [==========] 1 test from 1 test suite ran. (234287 ms total)
    [  PASSED  ] 1 test.
    
    ref:
    [1] https://source.android.com/devices/tech/perf/lmkd#userspace-lmkd-in-android-r
    [2] https://android.googlesource.com/device/google/coral/+/7804bc31dc72ce7bf5841da67f2d0db6740724f3%5E%21/#F0
    
    Change-Id: I7931f745b8b5568f4eb7a096304c3b9a701323e7

--- product_prop.mk
+++ product_prop.mk
@@ -27,6 +27,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     vendor.display.disable_rotator_downscale=1
 
+# LMKD
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.config.low_ram=false \
+    ro.lmk.log_stats=true
+
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.vc_call_vol_steps=7 \
