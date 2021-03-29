2f60239f3b68b56454a0300ced86620bcaca4667
daniml3 <danimoral1001@gmail.com>
2021-03-29 15:58:12 +0200

    vayu: build fastbootd
    Signed-off-by: daniml3 <danimoral1001@gmail.com>
    Change-Id: I14776da68eb2cf3e4a1a0c06da70f53a054da784

--- device.mk
+++ device.mk
@@ -10,6 +10,11 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Dynamic partitions setup
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd \
+    android.hardware.fastboot@1.0-impl-mock
+
 # File systems table
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
