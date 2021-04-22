d84b4d1c5daab440a68c06ac67c4288f11ea7532
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-04-22 11:59:16 +0200

    vayu: biometrics: Rebrand to vayu
    Change-Id: If1106b9567ec5e138e1404dc49f99d63f7fb6c84
    Signed-off-by: kubersharma001 <kubersharma001@gmail.com>

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -13,8 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service"
-#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service"
+#define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.vayu"
+#define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.vayu"
 
 #include <hardware/hw_auth_token.h>
 
