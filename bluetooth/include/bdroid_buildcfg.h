f94761955852b763200726bf6d5d14056f0fb912
kubersharma001 <kubersharma001@gmail.com>
2021-04-23 10:27:09 +0000

    vayu: Set default BT device name
    Change-Id: I3be6f144f92c7d54e6a171814feca109123e9353

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -21,6 +21,8 @@
 
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
+
+#define BTM_DEF_LOCAL_NAME "POCO X3 Pro"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
