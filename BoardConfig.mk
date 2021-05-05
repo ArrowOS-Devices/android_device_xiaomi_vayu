1f5816def168dbf77e0c5d45667e94cdfddc761b
kubersharma001 <kubersharma001@gmail.com>
2021-05-05 05:09:53 +0000

    vayu: Enforce Selinux
    Change-Id: Id9d3f70358eb75443dbb0bf82d10bd0dd1d3a260

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,7 +54,6 @@ BOARD_KERNEL_CMDLINE += androidboot.memcg=1 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PREBUILT_PATH)/dtbo.img
 BOARD_BOOTIMG_HEADER_VERSION := 2
