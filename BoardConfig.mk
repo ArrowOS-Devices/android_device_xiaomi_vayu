10bc72d15510e388df4665f08301b081c957c91e
Erfan Abdi <erfangplus@gmail.com>
2021-05-08 20:38:20 +0200

    vayu: Disable NFC for indian devices
    * Setup vayu sku when device is global.
    * Move the nfc manifest entry to vayu hardware sku
      variant.
    
    Change-Id: I76ac75cd80f85efa6dfa68753602d934a11000a3
    Signed-off-by: Arian <arian.kulmer@web.de>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -198,6 +198,8 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/frame
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/vintf/manifest.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/configs/vintf/manifest-qva.xml
+ODM_MANIFEST_SKUS += vayu
+ODM_MANIFEST_VAYU_FILES := $(DEVICE_PATH)/configs/vintf/manifest_vayu.xml
 
 # VNDK
 BOARD_VNDK_VERSION := current
