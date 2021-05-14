15d8ee4f373069280dacf018272f780b5a3ea40f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-05-14 15:28:59 +0530

    vayu: Kang SF phase offsets from coral
     * upon my testing, this seems to have fixed lags while switching
       from a lower to higher refresh rate with dynamic refresh rate
       and also reduced jitter and total duration in uibench
    
     * we wont be using the new android-11 offsets (phase offsets as
       duration) as caf surfaceflinger goes haywire and wrecks hwc
       when those are added
    
    This is also squashed with:
    Author: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Date:   Fri May 14 15:28:59 2021 +0530
    
        vayu:  Drop surfaceflinger vsync phase offsets
               * seems to be causing jank at 120hz
    
    [kubersharma001]:
    Partially reverts "Drop all SF offset related props."
    Partially reverts "Configure app/sf phase offset as duration props as per coral"
    Drop enable_advanced_sf_phase_offset prop
    
    Change-Id: I4b20336c677746b5c8ecbbb117e53a2dead5da21

--- properties/default.mk
+++ properties/default.mk
@@ -27,22 +27,13 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.use_color_management=true \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
-# Enable app/sf phase offset as durations. The numbers below are translated from the existing
-# positive offsets by finding the duration app/sf will have with the offsets.
-# For SF the previous value was 6ms which under 16.6ms vsync time (60Hz) will leave SF with ~10.5ms
-# for each frame. For App the previous value was 2ms which under 16.6ms vsync time will leave the
-# App with ~20.5ms (16.6ms * 2 - 10.5ms - 2ms). The other values were calculated similarly.
-# Full comparison between the old vs. the new values are captured in
-# https://docs.google.com/spreadsheets/d/1a_5cVNY3LUAkeg-yL56rYQNwved6Hy-dvEcKSxp6f8k/edit
-
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    debug.sf.use_phase_offsets_as_durations=1 \
-    debug.sf.late.sf.duration=10500000 \
-    debug.sf.late.app.duration=20500000 \
-    debug.sf.early.sf.duration=16000000 \
-    debug.sf.early.app.duration=16500000 \
-    debug.sf.earlyGl.sf.duration=13500000 \
-    debug.sf.earlyGl.app.duration=21000000
+    debug.sf.early_app_phase_offset_ns=500000 \
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.early_gl_phase_offset_ns=3000000 \
+    debug.sf.early_phase_offset_ns=500000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
+    debug.sf.high_fps_early_phase_offset_ns=6100000
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
