69138104549d4327ae034ff4e2fca277f2b9658d
kubersharma001 <kubersharma001@gmail.com>
2021-05-19 17:59:34 +0000

    vayu: Copy HotwordEnrollement privapp permissions to product
    * thanks xawlw for informing
    
    Change-Id: I1963bedbc5fb6b5117f66d01cd6c0a99fa01207b

--- device.mk
+++ device.mk
@@ -72,7 +72,7 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # Init scripts
 PRODUCT_PACKAGES += \
