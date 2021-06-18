3cef9f8fd1e98d325527059aa353f5926ab1f2f1
kubersharma001 <kubersharma001@gmail.com>
2021-06-18 06:12:27 +0000

    Revert "vayu: parts: Don't crash the whole process if dirac doesn't start"
    This reverts commit 10f736802ab16faca97195c972eb2cd5949e00eb.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -34,11 +34,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        try {
         DiracUtils.initialize(context);
-        } catch (Exception e) {
-            Log.d(TAG, "Dirac is not present in system");
-        }
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
     }
