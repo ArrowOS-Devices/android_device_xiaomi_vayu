1b9329b740e31d9ddd3c688c3c71fcc89c42f8ed
kubersharma001 <kubersharma001@gmail.com>
2021-06-21 16:42:30 +0000

    vayu: Import sec_config from CAF
    * LA.UM.9.1.r1-10200-SMxxx0.0
    
    Change-Id: I6f86642f692de922e0b7a1e7c736417daddbce3b

--- device.mk
+++ device.mk
@@ -235,6 +235,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.vayu
