0ac51463658f9d9f34d46efbb7ede6831d1d54d2
kubersharma001 <kubersharma001@gmail.com>
2021-07-15 23:43:35 +0530

    vayu: Switch to custom source compiled fingerprint HIDL

--- device.mk
+++ device.mk
@@ -184,6 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.vayu \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor \
     libkeymaster_messages.vendor
 
