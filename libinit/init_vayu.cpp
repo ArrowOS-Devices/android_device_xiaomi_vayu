94ea31ab8beb752c48bb3917a5851dc5bb48a7a6
kubersharma001 <kubersharma001@gmail.com>
2021-07-16 10:14:08 +0000

    vayu: libinit: Switch to walleye oreo build fingerprint.
    Change-Id: Ie346975d9691413f68f0486f7f8f84b3422c341f

--- libinit/init_vayu.cpp
+++ libinit/init_vayu.cpp
@@ -122,8 +122,8 @@ void set_device_props(const std::string fingerprint, const std::string descripti
 
 void vendor_load_properties() {
 //   SafetyNet workaround
-    char const fp[] = "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys";
-    char const fp_desc[] = "dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys";
+    char const fp[] = "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys";
+    char const fp_desc[] = "walleye-user 8.1.0 OPM1.171019.011 4448085 release-keys";
 
     string region = android::base::GetProperty("ro.boot.hwc", "");
 
