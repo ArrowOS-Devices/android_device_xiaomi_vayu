059162e6f91cc5acef13ebb0a872979df9fb1cc0
kubersharma001 <kubersharma001@gmail.com>
2021-07-21 11:44:57 +0000

    vayu: config.fs: Remove mappings for legacy root folders
    Change-Id: I12c3f63572da63720b5a6e794c81137bff3dafe7

--- configs/config.fs
+++ configs/config.fs
@@ -193,38 +193,8 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
-[firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[firmware/image/*]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
 [vendor/firmware_mnt/image/*]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
-caps: 0
-
-[bt_firmware/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[persist/]
-mode: 0771
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[dsp/]
-mode: 0771
-user: AID_MEDIA
-group: AID_MEDIA
 caps: 0
