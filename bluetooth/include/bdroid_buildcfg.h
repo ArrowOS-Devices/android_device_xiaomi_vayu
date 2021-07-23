e9cc3a1af5f23e53ac664e8491ebf92437450c7d
kubersharma001 <kubersharma001@gmail.com>
2021-07-23 05:17:14 +0000

    Revert "vayu: BT: Switch to bdroid_buildcfg.h for qti bt stack"
    This reverts commit b0f7ac4b3412acca4042279452230b4f79eb1175.
    
    Change-Id: Id20be066303fc1ce19d4bb2f24f62e0e815a9785

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -25,8 +25,9 @@
 #define BTM_DEF_LOCAL_NAME "POCO X3 Pro"
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    32
+#define MAX_L2CAP_CHANNELS    16
 #define BLE_VND_INCLUDED   TRUE
 
-#define AVDT_NUM_SEPS 35
+/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
+#define AVDT_NUM_SEPS 12
 #endif
