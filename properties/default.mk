b49755d54573aa6f94c28dfa43f5d506d780f742
kubersharma001 <kubersharma001@gmail.com>
2021-07-28 12:03:40 +0000

    vayu: Move surface_flinger props to default
     * while at it reorder camera prop and remove a blank line.
    
    Change-Id: Ie84e1c60abe7437b71de5fbd4148f84576f32145

--- properties/default.mk
+++ properties/default.mk
@@ -14,6 +14,19 @@
 # limitations under the License.
 #
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.has_wide_color_display=false \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.set_idle_timer_ms=500 \
+    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
+
 # Enable app/sf phase offset as durations. The numbers below are translated from the existing
 # positive offsets by finding the duration app/sf will have with the offsets.
 # For SF the previous value was 6ms which under 16.6ms vsync time (60Hz) will leave SF with ~10.5ms
