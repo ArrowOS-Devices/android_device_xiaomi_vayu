355302e6f02afb961d7a49b283df1a99d86aa36d
kubersharma001 <kubersharma001@gmail.com>
2021-08-12 10:07:37 +0000

    vayu: Move privapp-permissions-hotword.xml inside permissions folder
    Change-Id: I633d7aec8f09e574d068d08b5a7e3f00eec84365

--- device.mk
+++ device.mk
@@ -226,7 +226,7 @@ PRODUCT_PACKAGES += \
 
 # HotwordEnrollement
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
 # Init scripts
 PRODUCT_PACKAGES += \
