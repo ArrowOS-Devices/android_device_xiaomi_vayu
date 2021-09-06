719c06d475d15d1f7183a752af17813426681c10
kubersharma001 <kubersharma001@gmail.com>
2021-09-06 13:44:23 +0000

    vayu: Update blobs from miui_VAYUGlobal_V12.5.4.0.RJUMIXM_d43dee18b9_11.0
    Change-Id: I0754938419bb6ffa3b0d380bbbf14e3affdbe2fc

--- arrow_vayu.mk
+++ arrow_vayu.mk
@@ -23,9 +23,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.2.0.RJUMIXM release-keys"
+    PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.4.0.RJUMIXM release-keys"
 
-BUILD_FINGERPRINT := POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.2.0.RJUMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.4.0.RJUMIXM:user/release-keys
 
 DEVICE_MAINTAINER := kubersharma001
 
