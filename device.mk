39776cc75d10bfb06bff8699cb5467e90f5ce3d7
kubersharma001 <kubersharma001@gmail.com>
2021-10-02 12:05:07 +0000

    vayu: Build GPS stack and copy configs
    Change-Id: Id59540db8bd0e23923bd0c85bbd32b7dbb981a26

--- device.mk
+++ device.mk
@@ -210,6 +210,28 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti.vendor
 
 # GPS
+PRODUCT_PACKAGES += \
+     android.hardware.gnss@2.1-impl-qti \
+     android.hardware.gnss@2.1-service-qti
+
+PRODUCT_PACKAGES += \
+     liblocation_api \
+     libgps.utils \
+     libbatching \
+     libgeofencing \
+     libloc_core \
+     libgnss
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/etc/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/gps/etc/gnss_antenna_info.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gnss_antenna_info.conf \
+    $(LOCAL_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(LOCAL_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(LOCAL_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
 
