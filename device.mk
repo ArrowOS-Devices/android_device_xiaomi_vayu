0f8fad6e482c818e517537c19e61a6190d312993
Marijn Suijten <marijn.suijten@somainline.org>
2021-10-06 22:44:02 +0200

    vayu: Provide android.system.net.netd@1.1 on vendor for netmgrd
    Following the previous commit, starting with Android S most HAL
    libraries are not included in VNDK anymore - they need to be explicitly
    pulled in to `/vendor` for odm blobs to be allowed to access them:
    
        linker  : CANNOT LINK EXECUTABLE "/odm/bin/netmgrd": library "android.system.net.netd@1.1.so" not found: needed by main executable
    
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>
    Change-Id: Ide8adad466a61412a984a46d1562f8f2f6a62be3

--- device.mk
+++ device.mk
@@ -357,6 +357,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# Network
+PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1.vendor
+
 # Overlays
 PRODUCT_PACKAGES += \
    CarrierConfigOverlayVayu \
