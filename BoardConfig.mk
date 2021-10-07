77c46cc92bb770f486cfbc4b8c57ac132ded3c25
kubersharma001 <kubersharma001@gmail.com>
2021-10-07 10:28:36 +0000

    vayu: Unban ELF prebuilts in PRODUCT_COPY_FILES
    Change-Id: I3f8fc5ce5d1f7c8748c13ca66f8eca2ee8f90857

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,7 @@ DEVICE_PATH := device/xiaomi/vayu
 BOARD_VENDOR := xiaomi
 
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 # Architecture
 TARGET_ARCH := arm64
