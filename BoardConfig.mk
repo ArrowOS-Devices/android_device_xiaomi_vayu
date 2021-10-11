3f8ec8fe194c5daf396bcb5b4f1abee4d5b44512
kubersharma001 <kubersharma001@gmail.com>
2021-10-11 18:46:46 +0000

    vayuL BOARD_PLAT_* -> SYSTEM_EXT_*
    Change-Id: Id68a982a953f722afd75fff08cf14d65b80213f9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -174,8 +174,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor Security patch level
