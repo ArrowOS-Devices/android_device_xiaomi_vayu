04220fbddcaa5b4b245967fcc1e2dad3b2004772
kubersharma001 <kubersharma001@gmail.com>
2021-11-19 05:19:48 +0000

    vayu: Sync idle/touch timer to raven
    Change-Id: Ic4b295edc9bff00ad8d0bdbeb80f1340e3b288b2

--- properties/default.mk
+++ properties/default.mk
@@ -23,8 +23,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
-    ro.surface_flinger.set_idle_timer_ms=500 \
-    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.set_idle_timer_ms=80 \
+    ro.surface_flinger.set_touch_timer_ms=200 \
     ro.surface_flinger.use_color_management=true \
     ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
