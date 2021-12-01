11dd6081525a2b9adf918d78b3ea095e127b3359
brixia98 <darkdragon98.bt@gmail.com>
2021-12-01 20:22:01 +0100

    Vayu:initial awaken build

--- awaken_vayu.mk
+++ awaken_vayu.mk
@@ -11,10 +11,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Awaken OS stuff.
+$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_vayu
+PRODUCT_NAME := awaken_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -29,5 +29,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys
 
-DEVICE_MAINTAINER := kubersharma001
+DEVICE_MAINTAINER := brixia98
 
+# GMS
+TARGET_GAPPS_ARCH := arm64
+USE_GAPPS := true
