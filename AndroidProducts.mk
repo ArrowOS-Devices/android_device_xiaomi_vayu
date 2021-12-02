69a12fa76992f261dde229dfb99bbbe2389f81e7
brixia98 <darkdragon98.bt@gmail.com>
2021-12-02 12:01:58 +0100

    vayu: initial awaken build

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_vayu.mk
+    $(LOCAL_DIR)/awaken_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_vayu-eng \
-    arrow_vayu-userdebug \
-    arrow_vayu-user
+    awaken_vayu-eng \
+    awaken_vayu-userdebug \
+    awaken_vayu-user
