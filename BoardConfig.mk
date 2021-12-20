593e538038a3bcb83b4c4afe13855f7499f30a58
Nauval Rizky <enuma.alrizky@gmail.com>
2021-12-20 09:21:01 +0700

    vayu: power: Add custom boost extension for scroll handling
    Currently, we're not using CAF's BoostFramework due to having a pure
    AOSP based source, thus qcom perf hinting are not correctly applied.
    This extension will handle the vertical fling through device-specific
    boost override, utilizing existing libqti-perfd hinting.
    So any further configuration should be happens inside vertical scroll
    config inside perfboostsconfig.xml
    
    Reference: vendor/qcom/opensource/power/power-6150.c w/ reduced
    max interactive timeout
    
    Change-Id: I823c9383041b68ff2ce62d5f1a1c5091b2dfac15

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -132,6 +132,7 @@ BOARD_USES_METADATA_PARTITION := true
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWERHAL_BOOST_EXT := $(DEVICE_PATH)/power/boost-ext.cpp
 
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
---
+++ power/boost-ext.cpp
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2020-2021 The LineageOS Project
+ * Copyright (C) 2020-2021 Chaldeaprjkt
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * *    * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ *       copyright notice, this list of conditions and the following
+ *       disclaimer in the documentation and/or other materials provided
+ *       with the distribution.
+ *     * Neither the name of The Linux Foundation nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#define LOG_TAG "QTI PowerHAL"
+
+#include <log/log.h>
+
+#include <aidl/android/hardware/power/BnPower.h>
+
+extern "C" {
+
+#include "hint-data.h"
+#include "metadata-defs.h"
+#include "performance.h"
+#include "power-common.h"
+#include "utils.h"
+
+const int kMinInteractiveDuration = 100;  /* ms */
+const int kMaxInteractiveDuration = 2500; /* ms */
+
+static int process_interaction_hint(void* data) {
+    static struct timespec s_previous_boost_timespec;
+    static int s_previous_duration = 0;
+    static int interaction_handle = -1;
+
+    struct timespec cur_boost_timespec;
+    long long elapsed_time;
+    int duration = kMinInteractiveDuration;
+
+    if (data) {
+        int input_duration = *((int*)data);
+        if (input_duration > duration) {
+            duration = (input_duration > kMaxInteractiveDuration) ? kMaxInteractiveDuration
+                                                                  : input_duration;
+        }
+    }
+
+    clock_gettime(CLOCK_MONOTONIC, &cur_boost_timespec);
+
+    elapsed_time = calc_timespan_us(s_previous_boost_timespec, cur_boost_timespec);
+    // don't hint if it's been less than 250ms since last boost
+    // also detect if we're doing anything resembling a fling
+    // support additional boosting in case of flings
+    if (elapsed_time < 250000 && duration <= 750) {
+        return HINT_HANDLED;
+    }
+    s_previous_boost_timespec = cur_boost_timespec;
+    s_previous_duration = duration;
+
+    if (CHECK_HANDLE(interaction_handle)) {
+        release_request(interaction_handle);
+    }
+
+    interaction_handle = perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST,
+                                                    duration, SCROLL_VERTICAL);
+    if (!CHECK_HANDLE(interaction_handle)) {
+        ALOGE("Failed to perform interaction boost");
+        return HINT_NONE;
+    }
+    return HINT_HANDLED;
+}
+}
+
+using ::aidl::android::hardware::power::Boost;
+
+namespace aidl {
+namespace android {
+namespace hardware {
+namespace power {
+namespace impl {
+
+bool isDeviceSpecificBoostSupported(Boost type, bool *_aidl_return) {
+    switch (type) {
+        case Boost::INTERACTION:
+            *_aidl_return = true;
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool setDeviceSpecificBoost(Boost type, int32_t durationMs) {
+    switch (type) {
+        case Boost::INTERACTION:
+            process_interaction_hint(&durationMs);
+            return true;
+        default:
+            return false;
+    }
+}
+
+} // namespace impl
+} // namespace power
+} // namespace hardware
+} // namespace android
+} // namespace aidl
