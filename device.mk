f454c05d9dac182b7a3515ccde16e5b4afe0ed59
kubersharma001 <kubersharma001@gmail.com>
2022-01-02 07:44:45 +0000

    vayu: Remove non-existent libldacBT_{bco,dec} build rules
    - These seem to be long gone/not available to be source compiled?
    - Anyways, these seem to have no actual function and are not needed other than to avoid logspams
    
    unknown target 'libldacBT_dec'
    unknown target 'libldacBT_bco'
    
    This reverts "vayu: Compile libldacBT_bco.so" as I assumed it as valid build target
    
    Change-Id: Ia65d4e72128352132a7ad1a00c61d9596ece714a

--- device.mk
+++ device.mk
@@ -85,8 +85,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
-    libldacBT_dec \
-    libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
