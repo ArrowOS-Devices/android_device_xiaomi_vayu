7ce9e23986a095e2aca27ee4596f45d510b8bbd6
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    vayu: Removed audio A2dp from device.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba

--- device.mk
+++ device.mk
@@ -36,7 +36,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
-    audio.a2dp.default \
     audio.primary.msmnile \
     audio.r_submix.default \
     audio.usb.default \
