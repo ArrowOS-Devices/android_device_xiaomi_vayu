6ef8090616ad78417734eae3423ea69bdcbf595d
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-04-15 22:34:28 -0500

    Update and rename arrow_vayu.mk to bliss_vayu.mk

--- bliss_vayu.mk
+++ bliss_vayu.mk
@@ -12,9 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/bliss/config/common.mk)
 
-PRODUCT_NAME := arrow_vayu
+# boot animation fix I guess
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := bliss_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -29,5 +32,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:11/RKQ1.200826.002/V12.5.7.0.RJUMIXM:user/release-keys
 
-DEVICE_MAINTAINER := kubersharma001
 
