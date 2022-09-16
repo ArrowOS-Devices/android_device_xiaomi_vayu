b1c4ebf928592905baaaa871c0e90f7c27437119
kubersharma001 <kubersharma001@gmail.com>
2022-09-16 18:41:26 +0530

    vayu: Change kernel config name
    Change-Id: I5d7b2c3a2c52ca768fecb26c96a76c9c9a0c770b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,7 +87,7 @@ BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-  TARGET_KERNEL_CONFIG := vayu_user_defconfig
+  TARGET_KERNEL_CONFIG := vayu_defconfig
   TARGET_KERNEL_CLANG_COMPILE := true
   TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
   TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
