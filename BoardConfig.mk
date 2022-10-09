03f6cbb547d1c01a7d6527bd4af681a149d97e36
kubersharma001 <kubersharma001@gmail.com>
2022-10-09 21:45:39 +0530

    vayu: Extract from miui_VAYUGlobal_V13.0.5.0.SJUMIXM
     - Bump vendor security patch date
     - Update build desc,fp
    
    Change-Id: I9a7f59d8fcf24bde42203d32883c95a7655eae6a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,7 +175,7 @@ SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor Security patch level
-VENDOR_SECURITY_PATCH := 2022-02-01
+VENDOR_SECURITY_PATCH := 2022-07-01
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
