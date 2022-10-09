947f54f359540193fe19efc9f3c61f1555d6b86f
raghavt20 <raghavt20@aospa.co>
2022-10-10 00:03:22 +0530

    vayu: perf: Import perfconfigstore from vendor tree
    Change-Id: I1ecffad7f5e252442c835ac0357eb3efaf8dae6f

--- device.mk
+++ device.mk
@@ -388,7 +388,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml \
+    $(LOCAL_PATH)/configs/perf/perfconfigstore.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfconfigstore.xml
 
 # Public libraries
 PRODUCT_COPY_FILES += \
