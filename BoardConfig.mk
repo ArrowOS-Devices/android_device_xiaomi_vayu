d383da42f161ef8926b89202cf042a961e45a86a
kubersharma001 <kubersharma001@gmail.com>
2022-12-30 12:15:51 +0530

    vayu: Switch to sepolicy_vndr-legacy-um

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,7 +168,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 
 # Sepolicy
 SELINUX_IGNORE_NEVERALLOWS := true
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
