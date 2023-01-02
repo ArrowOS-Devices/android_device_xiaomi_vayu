17a48aad96dd84744f6282c7556de730b6bd19fa
kubersharma001 <kubersharma001@gmail.com>
2023-01-02 20:51:36 +0530

    vayu: Update maintainers list

--- arrow_vayu.mk
+++ arrow_vayu.mk
@@ -30,5 +30,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys
 
 # ArrowOS additions
-DEVICE_MAINTAINER := kubersharma001, raghavt20
+DEVICE_MAINTAINER := K001droid and raghavt20
 
