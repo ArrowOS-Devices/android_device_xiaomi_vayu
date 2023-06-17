fbeb347d05d03de1751a800b62a562c2d3511f42
Kuber <kubersharma001@gmail.com>
2023-06-17 10:22:09 +0530

    vayu: Remove duplicate prop
    error: found duplicate sysprop assignments:
    debug.sf.frame_rate_multiple_threshold=60
    debug.sf.frame_rate_multiple_threshold=120

--- properties/default.mk
+++ properties/default.mk
@@ -15,7 +15,6 @@
 #
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    debug.sf.frame_rate_multiple_threshold=120 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.has_HDR_display=true \
